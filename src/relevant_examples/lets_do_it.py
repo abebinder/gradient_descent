680dd2023216abdeb2bf91fe9dacf5e5c9bca3e8
Abe Binder <abe.binder@coloradocollege.edu>
2019-03-11 12:54:10 -0600

    correct mesh size for x squared plus y squared

--- src/relevant_examples/lets_do_it.py
+++ src/relevant_examples/lets_do_it.py
@@ -17,6 +17,8 @@ grad = GradientDescent()
 n_radii = 8
 n_angles = 36
 
+xguess=[2,2]
+
 # Make radii and angles spaces (radius r=0 omitted to eliminate duplication).
 radii = np.linspace(0.125, 1.0, n_radii)
 angles = np.linspace(0, 2*np.pi, n_angles, endpoint=False)
@@ -27,11 +29,17 @@ angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
 # Convert polar (radii, angles) coords to cartesian (x, y) coords.
 # (0, 0) is manually added at this stage,  so there will be no duplicate
 # points in the (x, y) plane.
-x = np.append(0, (radii*np.cos(angles)).flatten())
-y = np.append(0, (radii*np.sin(angles)).flatten())
+x = np.append(0, (radii*np.cos(angles)).flatten())* (xguess[0]*1.5)
+y = np.append(0, (radii*np.sin(angles)).flatten())* (xguess[1] *1.5)
 
 # Compute z to make the pringle surface.
 z = x**2+y**2
+
+print(x)
+print(y)
+
+print(z)
+
 plt.ion()
 fig = plt.figure()
 
@@ -43,8 +51,10 @@ ylist = rlist * np.cos(theta)
 
 
 
+
 def f(x):
     return x[0]**2 + x[1]**2
+
 
 
 for i in range(80,len(z),10):
@@ -52,19 +62,17 @@ for i in range(80,len(z),10):
     ax = fig.gca(projection='3d')
     ax.plot_trisurf(x[:i], y[:i], z[:i], linewidth=0.2, antialiased=True)
     # ax.plot(xlist[:i], ylist[:i], zlist[:i], label='parametric curve')
-    print(i)
     #ax.plot(np.zeros(i), np.zeros(i), zlist[:i], label='parametric curve')
     plt.pause(.0005)
 
 
 
-xguess=[20,20]
 for i in range(0,10000):
     ax = fig.gca(projection='3d')
     newxguess = grad.gradient_single_step(f,xguess,.005,100)[0]
     ax.plot([xguess[0]]+ [newxguess[0]], [xguess[1]] + [newxguess[1]], [f(xguess)] + [f(newxguess)] )
     xguess = newxguess
-    plt.pause(5)
+    plt.pause(.2)
 
 
 
